a76b9b7b218dc66150d93ef82ab5a589c7734511
chez-shanpu <cheztomo513@gmail.com>
2020-08-07 16:32:31 +0900

    WIP

--- controllers/persistentvolumeclaim_controller.go
+++ controllers/persistentvolumeclaim_controller.go
@@ -2,6 +2,10 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	storagev1 "k8s.io/api/storage/v1"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
@@ -17,20 +21,58 @@ type PersistentVolumeClaimReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=get;list;watch;update;patch
+// +kubebuilder:rbac:groups=storage.k8s.io,resources=storageclasses,verbs=get;list;watch
 
 func (r *PersistentVolumeClaimReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	ctx := context.Background()
 	_ = r.Log.WithValues("persistentvolumeclaim", req.NamespacedName)
 
 	// your logic here
+	var pvc corev1.PersistentVolumeClaim
+	err := r.Get(ctx, req.NamespacedName, &pvc)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	if pvc.Spec.StorageClassName == nil {
+		return ctrl.Result{}, errors.New("`pvc.spec.StorageClassName` should not be empty")
+	}
+
+	var sc storagev1.StorageClass
+	err = r.Get(ctx, client.ObjectKey{Name: *pvc.Spec.StorageClassName}, &sc)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *PersistentVolumeClaimReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	pred := predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			pvc := e.Object.(*corev1.PersistentVolumeClaim)
+			return filterPVC(pvc)
+		},
+		DeleteFunc: func(event.DeleteEvent) bool { return false },
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			pvc := e.ObjectNew.(*corev1.PersistentVolumeClaim)
+			return filterPVC(pvc)
+		},
+		GenericFunc: func(event.GenericEvent) bool { return true },
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1.PersistentVolumeClaim{}).
+		WithEventFilter(pred).
 		Complete(r)
+}
+
+func filterPVC(pvc *corev1.PersistentVolumeClaim) bool {
+	if pvc.Spec.Resources.Limits.Storage() == nil {
+		return false
+	}
+	if pvc.Spec.VolumeMode != nil && *pvc.Spec.VolumeMode != corev1.PersistentVolumeFilesystem {
+		return false
+	}
+	return true
 }
