a46c24bc09fc816111b63a07e498ff776bde8f61
chez-shanpu <cheztomo513@gmail.com>
2020-08-13 18:51:43 +0900

    Add test for convertSizeInBytes

---
+++ runners/pvc_autoresizer_test.go
@@ -0,0 +1,94 @@
+package runners
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("test resizer", func() {
+	Context("test convertSizeInBytes", func() {
+		type input struct {
+			valStr     string
+			capacity   int64
+			defaultVal string
+		}
+		type testCase struct {
+			input  input
+			expect int64
+		}
+		correctCases := []testCase{
+			{
+				input: input{
+					valStr:     "",
+					capacity:   100,
+					defaultVal: "10%",
+				},
+				expect: 10,
+			},
+			{
+				input: input{
+					valStr:     "20%",
+					capacity:   100,
+					defaultVal: "10%",
+				},
+				expect: 20,
+			},
+			{
+				input: input{
+					valStr:     "30Gi",
+					capacity:   40 << 30,
+					defaultVal: "10%",
+				},
+				expect: 30 << 30,
+			},
+			{
+				input: input{
+					valStr:     "100%",
+					capacity:   100,
+					defaultVal: "10%",
+				},
+				expect: 100,
+			},
+		}
+		errorCases := []input{
+			{
+				valStr:     "-10%",
+				capacity:   100,
+				defaultVal: "10%",
+			},
+			{
+				valStr:     "-10Gi",
+				capacity:   100,
+				defaultVal: "10%",
+			},
+			{
+				valStr:     "101%",
+				capacity:   100,
+				defaultVal: "10%",
+			},
+			{
+				valStr:     "11Gi",
+				capacity:   10 << 30,
+				defaultVal: "10%",
+			},
+			{
+				valStr:     "hoge",
+				capacity:   100,
+				defaultVal: "10%",
+			},
+		}
+		It("should be ok", func() {
+			for _, val := range correctCases {
+				res, err := convertSizeInBytes(val.input.valStr, val.input.capacity, val.input.defaultVal)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(res).To(Equal(val.expect))
+			}
+		})
+		It("should be error", func() {
+			for _, val := range errorCases {
+				_, err := convertSizeInBytes(val.valStr, val.capacity, val.defaultVal)
+				Expect(err).To(HaveOccurred())
+			}
+		})
+	})
+})
