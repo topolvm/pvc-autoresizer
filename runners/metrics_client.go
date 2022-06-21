topolvm/pvc-autoresizer

commit f7e1be3ceb9ee1e6a5c8cb15852fc87716054f82

Add inode checking feature
Add testcase

Fix Makefile

Fix tests

Fix annotation check process

Add testcase

Fix for btrfs

Add testcase

Add e2e test

Fix test case parameter(inodesThreshold)

Add inode infomations to the log message

Improve test case

Enforce percent validation

go mod tidy

runners/metrics_client.go
@@ -15,6 +15,8 @@ import (
 const (
 	volumeAvailableQuery = "kubelet_volume_stats_available_bytes"
 	volumeCapacityQuery  = "kubelet_volume_stats_capacity_bytes"
+	inodesAvailableQuery = "kubelet_volume_stats_inodes_free"
+	inodesCapacityQuery  = "kubelet_volume_stats_inodes"
 )
 
 // NewPrometheusClient returns a new prometheusClient
@@ -39,8 +41,10 @@ type MetricsClient interface {
 
 // VolumeStats is a struct containing metrics used by pvc-autoresizer
 type VolumeStats struct {
-	AvailableBytes int64
-	CapacityBytes  int64
+	AvailableBytes     int64
+	CapacityBytes      int64
+	AvailableInodeSize int64
+	CapacityInodeSize  int64
 }
 
 type prometheusClient struct {
@@ -60,12 +64,28 @@ func (c *prometheusClient) GetMetrics(ctx context.Context) (map[types.Namespaced
 		return nil, err
 	}
 
+	availableInodeSize, err := c.getMetricValues(ctx, inodesAvailableQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	capacityInodeSize, err := c.getMetricValues(ctx, inodesCapacityQuery)
+	if err != nil {
+		return nil, err
+	}
+
 	for key, val := range availableBytes {
 		vs := &VolumeStats{AvailableBytes: val}
 		if cb, ok := capacityBytes[key]; !ok {
 			continue
 		} else {
 			vs.CapacityBytes = cb
+		}
+		if ais, ok := availableInodeSize[key]; ok {
+			vs.AvailableInodeSize = ais
+		}
+		if cis, ok := capacityInodeSize[key]; ok {
+			vs.CapacityInodeSize = cis
 		}
 		volumeStatsMap[key] = vs
 	}

runners/pvc_autoresizer.go
@@ -148,6 +148,13 @@ func (w *pvcAutoresizer) resize(ctx context.Context, pvc *corev1.PersistentVolum
 		return nil
 	}
 
+	inodesThreshold, err := convertSize(pvc.Annotations[ResizeInodesThresholdAnnotation], vs.CapacityInodeSize, DefaultInodesThreshold)
+	if err != nil {
+		log.V(logLevelWarn).Info("failed to convert threshold annotation", "error", err.Error())
+		// lint:ignore nilerr ignores this because invalid annotations should be allowed.
+		return nil
+	}
+
 	curReq := pvc.Spec.Resources.Requests[corev1.ResourceStorage]
 	increase, err := convertSizeInBytes(pvc.Annotations[ResizeIncreaseAnnotation], curReq.Value(), DefaultIncrease)
 	if err != nil {
@@ -170,7 +177,7 @@ func (w *pvcAutoresizer) resize(ctx context.Context, pvc *corev1.PersistentVolum
 		}
 	}
 
-	if threshold > vs.AvailableBytes {
+	if threshold > vs.AvailableBytes || inodesThreshold > vs.AvailableInodeSize {
 		if pvc.Annotations == nil {
 			pvc.Annotations = make(map[string]string)
 		}
@@ -201,6 +208,8 @@ func (w *pvcAutoresizer) resize(ctx context.Context, pvc *corev1.PersistentVolum
 			"to", newReq.Value(),
 			"threshold", threshold,
 			"available", vs.AvailableBytes,
+			"inodesThreshold", inodesThreshold,
+			"inodesAvailable", vs.AvailableInodeSize,
 		)
 		w.recorder.Eventf(pvc, corev1.EventTypeNormal, "Resized", "PVC volume is resized to %s", newReq.String())
 	}
@@ -249,18 +258,8 @@ func convertSizeInBytes(valStr string, capacity int64, defaultVal string) (int64
 	if len(valStr) == 0 {
 		valStr = defaultVal
 	}
-
 	if strings.HasSuffix(valStr, "%") {
-		rate, err := strconv.ParseFloat(strings.TrimRight(valStr, "%"), 64)
-		if err != nil {
-			return 0, err
-		}
-		if rate < 0 {
-			return 0, fmt.Errorf("annotation value should be positive: %s", valStr)
-		}
-
-		res := int64(float64(capacity) * rate / 100.0)
-		return res, nil
+		return calcSize(valStr, capacity)
 	}
 
 	quantity, err := resource.ParseQuantity(valStr)
@@ -272,6 +271,29 @@ func convertSizeInBytes(valStr string, capacity int64, defaultVal string) (int64
 		return 0, fmt.Errorf("annotation value should be positive: %s", valStr)
 	}
 	return val, nil
+}
+
+func convertSize(valStr string, capacity int64, defaultVal string) (int64, error) {
+	if len(valStr) == 0 {
+		valStr = defaultVal
+	}
+	if strings.HasSuffix(valStr, "%") {
+		return calcSize(valStr, capacity)
+	}
+	return 0, fmt.Errorf("annotation value should be in percent notation: %s", valStr)
+}
+
+func calcSize(valStr string, capacity int64) (int64, error) {
+	rate, err := strconv.ParseFloat(strings.TrimRight(valStr, "%"), 64)
+	if err != nil {
+		return 0, err
+	}
+	if rate < 0 || rate > 100 {
+		return 0, fmt.Errorf("annotation value should between 0 and 100: %s", valStr)
+	}
+
+	res := int64(float64(capacity) * rate / 100.0)
+	return res, nil
 }
 
 func pvcStorageLimit(pvc *corev1.PersistentVolumeClaim) (resource.Quantity, error) {

Makefile
@@ -125,7 +125,7 @@ nilerr: ## Install nilerr
 .PHONY: setup
 setup: # Setup tools
 	mkdir -p bin
-	curl -sfL https://go.kubebuilder.io/dl/$(KUBEBUILDER_VERSION)/$(GOOS)/$(GOARCH) > $(BINDIR)/kubebuilder
+	curl -sfL https://github.com/kubernetes-sigs/kubebuilder/releases/download/v$(KUBEBUILDER_VERSION)/kubebuilder_$(GOOS)_$(GOARCH) > $(BINDIR)/kubebuilder
 	GOBIN=$(BINDIR) go install sigs.k8s.io/controller-tools/cmd/controller-gen@v$(CTRLTOOLS_VERSION)
 	curl -o $(BINDIR)/kubectl -sfL https://storage.googleapis.com/kubernetes-release/release/v$(ENVTEST_K8S_VERSION)/bin/linux/amd64/kubectl
 	chmod a+x $(BINDIR)/kubectl

e2e/suite_test.go
@@ -11,6 +11,9 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -159,7 +162,7 @@ var _ = Describe("pvc-autoresizer", func() {
 		increase := "1Gi"
 		storageLimit := ""
 
-		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, increase, storageLimit)
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, "", increase, storageLimit)
 
 		By("create a file with a size that does not exceed threshold disk usage")
 		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "fallocate", "-l", "400M", "/test1/test1.txt")
@@ -193,7 +196,7 @@ var _ = Describe("pvc-autoresizer", func() {
 		increase := ""
 		storageLimit := ""
 
-		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, increase, storageLimit)
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, "", increase, storageLimit)
 
 		By("create a file with a size that does not exceed threshold disk usage")
 		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "fallocate", "-l", "9G", "/test1/test1.txt")
@@ -213,7 +216,7 @@ var _ = Describe("pvc-autoresizer", func() {
 		increase := "1Gi"
 		storageLimit := "2Gi"
 
-		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, increase, storageLimit)
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, "", increase, storageLimit)
 
 		By("create a file with a size that exceed threshold disk usage")
 		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "fallocate", "-l", "600M", "/test1/test1.txt")
@@ -240,7 +243,7 @@ var _ = Describe("pvc-autoresizer", func() {
 		increase := "1Gi"
 		storageLimit := ""
 
-		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, increase, storageLimit)
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, "", increase, storageLimit)
 
 		By("write data with a size that exceed threshold disk usage")
 		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "dd", "if=/dev/zero", "of=/dev/e2etest", "count=600M", "iflag=count_bytes")
@@ -260,7 +263,7 @@ var _ = Describe("pvc-autoresizer", func() {
 		increase := "1Gi"
 		storageLimit := ""
 
-		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, increase, storageLimit)
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, "", increase, storageLimit)
 
 		By("create a file with a size that exceed threshold disk usage")
 		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "fallocate", "-l", "600M", "/test1/test1.txt")
@@ -269,9 +272,54 @@ var _ = Describe("pvc-autoresizer", func() {
 		By("checking the disk does not resize")
 		checkDoesNotResize(pvcName, "1Gi")
 	})
+
+	It("should resize PVC based on PVC spec and annotations(inode)", func() {
+		pvcName := "test-pvc"
+		sc := "topolvm-provisioner-annotated"
+		mode := string(corev1.PersistentVolumeFilesystem)
+		request := "1Gi"
+		limit := "2Gi"
+		threshold := "50%"
+		increase := "1Gi"
+		inodesThreshold := "99%"
+		storageLimit := ""
+
+		var capacityInode int64
+		var availableInode int64
+
+		resources = createPodPVC(resources, pvcName, sc, mode, pvcName, request, limit, threshold, inodesThreshold, increase, storageLimit)
+
+		By("getting available inode size and capacity inode size")
+		stdout, stderr, err := kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "df", "/test1", "--output=target,itotal,iavail")
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+		lines := regexp.MustCompile(`\n`).Split(string(stdout), -1)
+		Expect(len(lines)).Should(Equal(3))
+
+		cs := regexp.MustCompile(`\s+`).Split(string(lines[1]), -1)
+		capacityInode, err = strconv.ParseInt(cs[1], 10, 64)
+		Expect(err).ShouldNot(HaveOccurred())
+		availableInode, err = strconv.ParseInt(cs[2], 10, 64)
+		Expect(err).ShouldNot(HaveOccurred())
+		Expect(capacityInode).ShouldNot(Equal(int64(0)))
+		Expect(availableInode).ShouldNot(Equal(int64(0)))
+
+		By("checking the disk does not resize")
+		checkDoesNotResize(pvcName, request)
+
+		By("create files for consume an inodes")
+		rate, err := strconv.ParseFloat(strings.TrimRight(inodesThreshold, "%"), 64)
+		Expect(err).ShouldNot(HaveOccurred())
+		th := int64(float64(capacityInode) * rate / 100.0)
+		num := capacityInode - th
+		stdout, stderr, err = kubectl("-n", testNamespace, "exec", "-it", pvcName, "--", "bash", "-c", fmt.Sprintf("touch /test1/testfile_{0..%d}.txt", num))
+		Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s", stdout, stderr)
+
+		By("checking the disk resizing")
+		checkDiskResize(pvcName, "2Gi", true)
+	})
 })
 
-func buildPodPVCTemplateYAML(pvcName, storageClassName, volumeMode, podName, request, limit, threshold, increase, storageLimit string) ([]byte, error) {
+func buildPodPVCTemplateYAML(pvcName, storageClassName, volumeMode, podName, request, limit, threshold, inodesThreshold, increase, storageLimit string) ([]byte, error) {
 	var b bytes.Buffer
 	var err error
 
@@ -288,25 +336,26 @@ func buildPodPVCTemplateYAML(pvcName, storageClassName, volumeMode, podName, req
 	}
 
 	params := map[string]string{
-		"pvcName":                pvcName,
-		"storageClassName":       storageClassName,
-		"volumeMode":             volumeMode,
-		"podName":                podName,
-		"namespace":              testNamespace,
-		"useAnnotation":          useAnnotation,
-		"thresholdAnnotation":    threshold,
-		"increaseAnnotation":     increase,
-		"storageLimitAnnotation": storageLimit,
-		"resourceRequest":        request,
-		"resourceLimit":          limit,
+		"pvcName":                   pvcName,
+		"storageClassName":          storageClassName,
+		"volumeMode":                volumeMode,
+		"podName":                   podName,
+		"namespace":                 testNamespace,
+		"useAnnotation":             useAnnotation,
+		"thresholdAnnotation":       threshold,
+		"increaseAnnotation":        increase,
+		"inodesThresholdAnnotation": inodesThreshold,
+		"storageLimitAnnotation":    storageLimit,
+		"resourceRequest":           request,
+		"resourceLimit":             limit,
 	}
 	err = podPVCTmpl.Execute(&b, params)
 	return b.Bytes(), err
 }
 
-func createPodPVC(resources []resource, pvcName, storageClassName, volumeMode, podName, request, limit, threshold, increase, storageLimit string) []resource {
+func createPodPVC(resources []resource, pvcName, storageClassName, volumeMode, podName, request, limit, threshold, inodesThreshold, increase, storageLimit string) []resource {
 	By("create a PVC and a pod for test")
-	podPVCYAML, err := buildPodPVCTemplateYAML(pvcName, storageClassName, volumeMode, pvcName, request, limit, threshold, increase, storageLimit)
+	podPVCYAML, err := buildPodPVCTemplateYAML(pvcName, storageClassName, volumeMode, pvcName, request, limit, threshold, inodesThreshold, increase, storageLimit)
 	Expect(err).ShouldNot(HaveOccurred())
 	stdout, stderr, err := kubectlWithInput(podPVCYAML, "apply", "-f", "-")
 	Expect(err).ShouldNot(HaveOccurred(), "stdout=%s, stderr=%s yaml=\n%s", stdout, stderr, podPVCYAML)

e2e/testdata/pod-pvc-template.yaml
@@ -6,6 +6,7 @@ metadata:
   annotations:
     resize.topolvm.io/threshold: "{{ .thresholdAnnotation }}"
     resize.topolvm.io/increase: "{{ .increaseAnnotation }}"
+    resize.topolvm.io/inodes-threshold: "{{ .inodesThresholdAnnotation }}"
     resize.topolvm.io/storage_limit: "{{ .storageLimitAnnotation }}"
 spec:
   accessModes:

go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
 	github.com/prometheus/client_golang v1.11.0
-	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/client_model v0.2.0
 	github.com/prometheus/common v0.26.0
 	github.com/spf13/cobra v1.1.1
 	k8s.io/api v0.21.2

runners/constants.go
@@ -6,6 +6,9 @@ const AutoResizeEnabledKey = "resize.topolvm.io/enabled"
 // ResizeThresholdAnnotation is the key of resize threshold.
 const ResizeThresholdAnnotation = "resize.topolvm.io/threshold"
 
+// ResizeInodesThresholdAnnotation is the key of resize threshold for inodes.
+const ResizeInodesThresholdAnnotation = "resize.topolvm.io/inodes-threshold"
+
 // ResizeIncreaseAnnotation is the key of amount increased.
 const ResizeIncreaseAnnotation = "resize.topolvm.io/increase"
 
@@ -17,6 +20,9 @@ const PreviousCapacityBytesAnnotation = "resize.topolvm.io/pre_capacity_bytes"
 
 // DefaultThreshold is the default value of ResizeThresholdAnnotation.
 const DefaultThreshold = "10%"
+
+// DefaultInodesThreshold is the default value of ResizeInodesThresholdAnnotation.
+const DefaultInodesThreshold = "10%"
 
 // DefaultIncrease is the default value of ResizeIncreaseAnnotation.
 const DefaultIncrease = "10%"

commit 8d2a5712e509c37db72aeefa8251cdeb4ddb4cd9

reconcile: do not resize volume if failed to get inode stats
The pvc-autoresizer decides whether each volume needs to be resized
based on volume stats. This stats consist of available bytes, capacity,
free inode count and all inode counts. The controller gets these values
from prometheus metrics.

Without this patch, if metrics lack inodes counts for some target
volumes, decision on resize necessity are done as if the lacking values
are zero. This causes unnecessary resizes for such volumes, and may
continues to resize up to the volume storage limit.

This patch changes the reconciler to resize volumes only if the
controller succeeded to get all four volume stats.

Fixes: f7e1be3ceb9e ("Add inode checking feature")

runners/metrics_client.go
@@ -36,6 +36,11 @@ func NewPrometheusClient(url string) (MetricsClient, error) {
 
 // MetricsClient is an interface for getting metrics
 type MetricsClient interface {
+	// GetMetrics returns volume stats metrics of PVCs
+	//
+	// The volume stats consist of available bytes, capacity bytes, available inodes and capacity
+	// inodes. This method returns volume stats for a PVC only if all four metrics of the PVC was
+	// retrieved from the metrics source.
 	GetMetrics(ctx context.Context) (map[types.NamespacedName]*VolumeStats, error)
 }
 
@@ -51,6 +56,7 @@ type prometheusClient struct {
 	prometheusAPI prometheusv1.API
 }
 
+// GetMetrics implements MetricsClient.GetMetrics
 func (c *prometheusClient) GetMetrics(ctx context.Context) (map[types.NamespacedName]*VolumeStats, error) {
 	volumeStatsMap := make(map[types.NamespacedName]*VolumeStats)
 
@@ -76,16 +82,20 @@ func (c *prometheusClient) GetMetrics(ctx context.Context) (map[types.Namespaced
 
 	for key, val := range availableBytes {
 		vs := &VolumeStats{AvailableBytes: val}
-		if cb, ok := capacityBytes[key]; !ok {
-			continue
-		} else {
+		if cb, ok := capacityBytes[key]; ok {
 			vs.CapacityBytes = cb
+		} else {
+			continue
 		}
 		if ais, ok := availableInodeSize[key]; ok {
 			vs.AvailableInodeSize = ais
+		} else {
+			continue
 		}
 		if cis, ok := capacityInodeSize[key]; ok {
 			vs.CapacityInodeSize = cis
+		} else {
+			continue
 		}
 		volumeStatsMap[key] = vs
 	}

runners/pvc_autoresizer.go
@@ -118,10 +118,11 @@ func (w *pvcAutoresizer) reconcile(ctx context.Context) error {
 			return nil
 		}
 		for _, pvc := range pvcs.Items {
+			log := w.log.WithValues("namespace", pvc.Namespace, "name", pvc.Name)
 			isTarget, err := isTargetPVC(&pvc)
 			if err != nil {
 				metrics.ResizerFailedResizeTotal.Increment(pvc.Name, pvc.Namespace)
-				w.log.WithValues("namespace", pvc.Namespace, "name", pvc.Name).Error(err, "failed to check target PVC")
+				log.Error(err, "failed to check target PVC")
 				continue
 			} else if !isTarget {
 				continue
@@ -131,12 +132,22 @@ func (w *pvcAutoresizer) reconcile(ctx context.Context) error {
 				Name:      pvc.Name,
 			}
 			if _, ok := vsMap[namespacedName]; !ok {
+				// Do not increment ResizerFailedResizeTotal here. The controller cannot get volume
+				// stats for "offline" volumes (i.e. volumes not mounted by any pod) since kubelet
+				// exports volume stats of a persistent volume claim only if it is online. Besides,
+				// NodeExpandVolume RPC assumes that the volume to be published or staged on a node
+				// (and hence online), the resize request of controller for offline PVC will not be
+				// processed for the time being. So, we do not regard it as a resize failure that
+				// the controller failed to retrieve volume stats for the PVC. This may result in a
+				// failure to increment the counter in the case which the PVC is online but fails
+				// to retrieve its metrics, but accept this as a limitation for now.
+				log.Info("failed to get volume stats")
 				continue
 			}
 			err = w.resize(ctx, &pvc, vsMap[namespacedName])
 			if err != nil {
 				metrics.ResizerFailedResizeTotal.Increment(pvc.Name, pvc.Namespace)
-				w.log.WithValues("namespace", pvc.Namespace, "name", pvc.Name).Error(err, "failed to resize PVC")
+				log.Error(err, "failed to resize PVC")
 			}
 		}
 	}