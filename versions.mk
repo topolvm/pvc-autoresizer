0a87ec8b0dfe65169eb01c4dcdf228d6cd8a8956
Daichi Mukai <daichi-mukai@cybozu.co.jp>
2024-05-31 16:39:51 +0900

    Remove unused KIND_NODE_IMAGE variable in versions.mk
    The KIND_NODE_IMAGE variable in versions.mk file is not used. This
    commit removes this one.
    
    Signed-off-by: Daichi Mukai <daichi-mukai@cybozu.co.jp>

--- versions.mk
+++ versions.mk
@@ -24,11 +24,3 @@ ENVTEST_K8S_VERSION := $(shell echo $(KUBERNETES_VERSION) | cut -d "." -f 1-2)
 # Tools versions which are defined in go.mod
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 GINKGO_VERSION := $(shell awk '/github.com\/onsi\/ginkgo\/v2/ {print substr($$2, 2)}' $(SELF_DIR)/go.mod)
-
-ifeq ($(KUBERNETES_VERSION), 1.28.0)
-KIND_NODE_IMAGE=kindest/node:v1.28.0@sha256:b7a4cad12c197af3ba43202d3efe03246b3f0793f162afb40a33c923952d5b31
-else ifeq ($(KUBERNETES_VERSION),1.27.3)
-KIND_NODE_IMAGE=kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23ef8a83cba8a463690e98317add2c9ba72
-else ifeq ($(KUBERNETES_VERSION),1.26.6)
-KIND_NODE_IMAGE=kindest/node:v1.26.6@sha256:6e2d8b28a5b601defe327b98bd1c2d1930b49e5d8c512e1895099e4504007adb
-endif
