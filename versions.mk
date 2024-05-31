77e5792131c6dc9d6ba58161a97fdf2a6df61b2f
Daichi Mukai <daichi-mukai@cybozu.co.jp>
2024-05-31 17:03:57 +0900

    Support Kubernetes v1.29
    This commit adds support for Kubernetes v1.29 and drops support for
    Kubernetes v1.26.
    
    The author followed the procedures in maintenance.md, but there is one
    exception: we upgraded the Go version to Go 1.22 despite K8s v1.29 being
    built with Go 1.21. This is because TopoLVM that supports K8s v1.29
    requires Go 1.22 and it is hard to use the Go version that cannot build
    TopoLVM without rewriting the CI workflow a lot.
    
    Signed-off-by: Daichi Mukai <daichi-mukai@cybozu.co.jp>

--- versions.mk
+++ versions.mk
@@ -1,18 +1,18 @@
 # https://github.com/helm/chart-testing/releases
-CHART_TESTING_VERSION := 3.10.1
+CHART_TESTING_VERSION := 3.11.0
 # https://github.com/kubernetes-sigs/controller-tools/releases
-CONTROLLER_TOOLS_VERSION := 0.13.0
+CONTROLLER_TOOLS_VERSION := 0.14.0
 # https://github.com/golangci/golangci-lint/releases
-GOLANGCI_LINT_VERSION := v1.55.2
+GOLANGCI_LINT_VERSION := v1.59.0
 # https://github.com/norwoodj/helm-docs/releases
-HELM_DOCS_VERSION := 1.12.0
+HELM_DOCS_VERSION := 1.13.1
 # https://github.com/helm/helm/releases
-HELM_VERSION := 3.14.0
+HELM_VERSION := 3.15.1
 # https://github.com/prometheus-operator/kube-prometheus/releases
 KUBE_PROMETHEUS_VERSION := 0.13.0
 # It is set by CI using the environment variable, use conditional assignment.
-KUBERNETES_VERSION ?= 1.28.0
-TOPOLVM_VERSION := topolvm-chart-v14.0.0
+KUBERNETES_VERSION ?= 1.29.2
+TOPOLVM_VERSION := 658d9f0a3f2df6acb2aab7cb2d75065baaa62afc
 
 # Tools versions which are defined in go.mod
 SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
